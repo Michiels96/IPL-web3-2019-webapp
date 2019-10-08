1d2c96af5604c072aa9eaaea8bdea401e0c070d7
Raphael <raphi.baroni@gmail.com>
2019-10-08 16:30:30 +0200

    Slight update to include function names to be completed by students and to deal with errors in promise

--- src/components/Gallery/Gallery.container.jsx
+++ src/components/Gallery/Gallery.container.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import Gallery from "./Gallery";
 
 const DEFAULT_ITEMS = [
-  {    
-    picture:"welcome-keep-calm.jpg",
+  {
+    picture: "welcome-keep-calm.jpg",
     description: "This picture is one among..."
   },
   {
@@ -12,64 +12,72 @@ const DEFAULT_ITEMS = [
   }
 ];
 
-class GalleryContainer extends React.Component{
-  constructor(){
+class GalleryContainer extends React.Component {
+  constructor() {
     super();
     this.state = {
       items: [],
       formItem: {
         picture: "",
-        description: "",
+        description: ""
       }
-    }
+    };
 
     this.setNewItemText = this.setNewItemText.bind(this);
     this.setNewItemPicture = this.setNewItemPicture.bind(this);
     this.saveNewItem = this.saveNewItem.bind(this);
+    this.removeItem = this.removeItem.bind(this);
+    this.updateItem = this.updateItem.bind(this);
   }
 
   render() {
-      return <Gallery
+    return (
+      <Gallery
         items={this.state.items}
         formItem={this.state.formItem}
-
         setNewItemText={this.setNewItemText}
         setNewItemPicture={this.setNewItemPicture}
         saveNewItem={this.saveNewItem}
+        removeItem={this.removeItem}
+        updateItem={this.updateItem}
       />
+    );
   }
 
-  async componentDidMount(){
-    const items = await this.loadItems();
-    this.setState({
-      items: items,
+  async componentDidMount() {
+    try {
+      const items = await this.loadItems();
+      this.setState({
+        items: items
+      });
+    } catch (err) {
+      console.log("Error in comonentDidMount:", err);
+    }
+  }
+
+  loadItems() {
+    return new Promise((resolve, reject) => {
+      resolve(DEFAULT_ITEMS);
     });
   }
-
-
-  loadItems(){
-    return new Promise((resolve, reject) => {
-        resolve(DEFAULT_ITEMS);
-    })
-  }
-  setNewItemText(newValue){
-    const {
-      formItem
-    } = this.state;
+  setNewItemText(newValue) {
+    const { formItem } = this.state;
 
     formItem.description = newValue;
 
     console.log("GalleryContainer::setNewItemText", newValue);
 
     this.setState({
-      formItem: formItem,
-    })
+      formItem: formItem
+    });
   }
-  setNewItemPicture(){
+  //To be completed (arguments and function body)
+  setNewItemPicture() {}
+  saveNewItem() {}
+  removeItem() {}
+  updateItem() {
+    
   }
-  saveNewItem(){
-  }
-
 }
 
 export default GalleryContainer;
