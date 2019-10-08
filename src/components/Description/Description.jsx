431d08f4e96c5012684a4ccd12b843f8aada04cd
Raphael <raphi.baroni@gmail.com>
2019-10-08 18:00:39 +0200

    Buttons for update and delete item

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -8,7 +8,7 @@ function Description({
 }) { 
     //mx-2 mx-md-auto : center the description from medium viewport (even though the row align content from start)
     //m-2 my-md-auto : add margin for small viewport, else center the      
-    return <Col md={7} className="my-2 mx-2 mx-md-auto border border-secondary rounded-lg">{description}</Col>  
+    return <Col md={6} className="my-2 mx-2 mx-md-auto border border-secondary rounded-lg">{description}</Col>  
 }
 
 const improve = withNull("description", NullDescription);
--- src/components/Gallery/Item/GalleryItem.jsx
+++ src/components/Gallery/Item/GalleryItem.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Row } from "react-bootstrap";
+import { Row,Col,Button } from "react-bootstrap";
 import Picture from "../../Picture/Picture";
 import Description from "../../Description/Description";
 
@@ -12,6 +12,27 @@ function GalleryItem({
         <Row className="m-2 justify-content-start border border-dark rounded-lg">
             <Picture picture={picture} /> 
             <Description description={description} />
+            <Col className="col text-center">
+                <Button
+                id="save_item"
+                variant="btn btn-outline-dark"
+                className="m-2"
+                disabled                
+                >
+                Save
+                </Button>
+
+                <Button
+                    id="delete_item"
+                    variant="btn btn-outline-dark"
+                    className="m-2"
+                    disabled                    
+                >
+                    Delete
+                </Button>
+          </Col>
+        
+         
         </Row>        
     )
   
